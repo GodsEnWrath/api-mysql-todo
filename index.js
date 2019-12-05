9cfb4ba12b4f053ff93c45d9551f261cd0ee5e5b
GodsEnWrath <fastyazid@gmail.com>
2019-12-05 16:12:26 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const { PORT, connection } = require("./config")
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+    res.send({message: "Welcome to my sql api"})
+})
+
+app.use("/todos", require("./routes/todos"))
+app.use("/users", require("./routes/users"))
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("error connecting: " + err.stack)
+        return
+    }
+    console.log("connected to database")
+})
+
+app.listen(PORT, () => {
+    console.log(`This app listen to ${PORT} `)
+})
